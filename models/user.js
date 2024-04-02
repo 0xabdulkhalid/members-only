f9a160a21f8559210e6342979628db2ea7c2d802
Abdul Khalid <mezmerizingbeats2004@gmail.com>
2024-04-02 21:04:40 +0530

    refactor: user schema model

--- models/user.js
+++ models/user.js
@@ -2,11 +2,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  mail: { type: String, required: true },
-  password: { type: String, required: true },
-  isAdmin: { type: Boolean, required: true },
+  firstName: { type: String, required: true, maxLength: 20 },
+  lastName: { type: String, required: true, maxLength: 20 },
+  mail: { type: String, required: true, maxLength: 50 },
+  password: { type: String, required: true, maxLength: 25 },
+  isMember: { type: Boolean, required: true, default: false },
+  isAdmin: { type: Boolean, required: true, default: false },
 });
 
 const User = mongoose.model("User", UserSchema);
