6c2c0cebf491145e5206ddb88670ae4cebb5e9f6
Abdul Khalid <mezmerizingbeats2004@gmail.com>
2024-04-07 11:04:43 +0530

    feat: create message schema

---
+++ models/message.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const MessageSchema = new Schema({
+  text: { type: String, required: true, maxLength: 200 },
+  author: { type: String, required: true, maxLength: 40 },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const Message = mongoose.model("Message", MessageSchema);
+module.exports = Message;
